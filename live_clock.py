23a3df95b8908b0e43c693feec634e11dcaf06d8
Robert Parks <r1parks.ucsd@gmail.com>
2026-04-16 00:07:53 -0400

    moved mock class to its own module

--- live_clock.py
+++ live_clock.py
@@ -17,53 +17,7 @@ except ImportError:
     logging.warning(
         "rgbmatrix module not found. Using a mock implementation."
     )
-
-    class RGBMatrixOptions:
-        def __init__(self):
-            self.rows = 32
-            self.cols = 64
-            self.hardware_mapping = 'adafruit-hat'
-            self.drop_privileges = False
-
-    class RGBMatrix:
-        def __init__(self, options=None):
-            self.brightness = 100
-
-        def CreateFrameCanvas(self):
-            return MockCanvas()
-
-        def Clear(self):
-            pass
-
-        def SwapOnVSync(self, canvas):
-            return canvas
-
-    class MockCanvas:
-        def Clear(self):
-            pass
-
-        def SetPixel(self, x, y, r, g, b):
-            pass
-
-    class graphics:
-        class Color:
-            def __init__(self, r, g, b):
-                self.r, self.g, self.b = r, g, b
-
-        class Font:
-            def __init__(self):
-                pass
-
-            def LoadFont(self, path):
-                pass
-
-        @staticmethod
-        def DrawText(canvas, font, x, y, color, text):
-            pass
-
-        @staticmethod
-        def DrawLine(canvas, x1, y1, x2, y2, color):
-            pass
+    from matrix_mock import RGBMatrix, RGBMatrixOptions, graphics
 
 from config_manager import Config  # noqa: E402
 
@@ -409,7 +363,8 @@ class SubwayClock:
 
             weather_color = graphics.Color(255, 215, 0)
             graphics.DrawText(
-                self.canvas, self.small_font, 2, 31, weather_color, weather_text
+                self.canvas, self.small_font, 2, 31,
+                weather_color, weather_text
             )
             self.draw_time()
             self.canvas = self.matrix.SwapOnVSync(self.canvas)
